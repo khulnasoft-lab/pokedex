a5c70c32028a13162e69c16a4a2be608f647c404
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-05-25 12:05:13 +0600

    Create seed.js
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ seed.js
@@ -0,0 +1,42 @@
+const db = require("./src/db");
+const fs = require("fs/promises");
+const path = require("path");
+
+async function seedDb() {
+	const file = await fs.readFile(path.join(__dirname, "./data/pokedex.json"));
+	const pokedex = JSON.parse(file.toString());
+
+	for (let pokemon of pokedex) {
+		console.log({
+			...pokemon,
+			en: pokemon.name.english,
+			jp: pokemon.name.japanese,
+			ch: pokemon.name.chinese,
+			fr: pokemon.name.french,
+		});
+		await db
+			.put({
+				TableName: "kengine-pokedex-prod",
+				Item: {
+					...pokemon,
+                    game: 'pokemon',
+					en: pokemon.name.english,
+					jp: pokemon.name.japanese,
+					ch: pokemon.name.chinese,
+					fr: pokemon.name.french,
+				},
+			})
+			.promise();
+
+		await db.put({
+			TableName: "kengine-pokedex-prod-counter",
+			Item: {
+				id: "pokedex",
+				count: 1
+			}
+		}).promise()
+	}
+}
+
+seedDb();
+exports.handler = seedDb;
